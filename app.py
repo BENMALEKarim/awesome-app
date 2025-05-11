b731b9e6f60ca10fb82323657c6f2e1230eec069
BENMALEKarim <fk_benmalek@esi.dz>
2025-05-11 16:20:45 +0200

    add some style to app

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-import os 
-from flask import Flask
+from flask import Flask, render_template_string
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def welcome():
-    return 'Hello Karim and Accenture/Swisscom/Udemy CoP Devops: ' + os.environ.get('MESSAGE')
+# Get config from environment variables
+MESSAGE = "Hello from: " + os.getenv("APP_VERSION")
+BACKGROUND_COLOR = os.getenv("BACKGROUND_COLOR")
 
-app.run(debug=True, host='0.0.0.0', port=8080)
+TEMPLATE = """
+<!doctype html>
+<html lang="en">
+<head>
+  <title>Flask App</title>
+  <style>
+    body {
+      background-color: {{ bg_color }};
+      color: #333;
+      font-family: Arial, sans-serif;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      font-size: 2rem;
+    }
+  </style>
+</head>
+<body>
+  {{ message }}
+</body>
+</html>
+"""
+
+@app.route('/')
+def index():
+    return render_template_string(
+        TEMPLATE,
+        message=MESSAGE,
+        bg_color=BACKGROUND_COLOR
+    )
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
