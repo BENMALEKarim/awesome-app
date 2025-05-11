a21bfd17ff45718f94b3baa0e81ad353daf7ec56
BENMALEKarim <fk_benmalek@esi.dz>
2025-05-11 16:45:14 +0200

    update app

--- app.py
+++ app.py
@@ -7,37 +7,9 @@ app = Flask(__name__)
 MESSAGE = "Hello from version: " + os.getenv("APP_VERSION")
 BACKGROUND_COLOR = os.getenv("BACKGROUND_COLOR")
 
-TEMPLATE = """
-<!doctype html>
-<html lang="en">
-<head>
-  <title>Flask App</title>
-  <style>
-    body {
-      background-color: {{ bg_color }};
-      color: #333;
-      font-family: Arial, sans-serif;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-      font-size: 2rem;
-    }
-  </style>
-</head>
-<body>
-  {{ message }}
-</body>
-</html>
-"""
-
 @app.route('/')
 def index():
-    return render_template_string(
-        TEMPLATE,
-        message=MESSAGE,
-        bg_color=BACKGROUND_COLOR
-    )
+    return render_template("index.html", message=MESSAGE, bg_color=BACKGROUND_COLOR)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
